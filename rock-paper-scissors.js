9d8529895fe3808fd71128741d5e6f160bc4caab
Sehr Chowdhury <sehr.chowdhury@gmail.com>
2026-06-10 12:59:18 -0400

    commit

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -35,6 +35,17 @@ document.querySelector('.js-paper-button').addEventListener('click', () =>{
 document.querySelector('.js-scissors-button').addEventListener('click', () =>{
     playGame('scissors');
 });
+
+document.body.addEventListener('keydown', (event) => {
+    if(event.key === 'r'){
+        playGame('rock');
+    }else if(event.key === 'p'){
+        playGame('paper');
+    }else if(event.key === 's'){
+        playGame('scissors');
+    }
+});
+
 function playGame(playerMove) {
     const computerMove = pickComputerMove(); // step 1
     // step 3:
